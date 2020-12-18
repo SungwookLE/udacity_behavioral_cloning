4203dad3a48d05ae1b6b3c6a9ffab05b1db2a4c6
Sungwook Lee <joker1251@naver.com>
2020-12-19 03:20:39 +0900

    Update model.py
    ..cheerup

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ def generator(samples, batch_size=32):
         while 1: #Loop forever so the generator never terminates
                 sklearn.utils.shuffle(samples) #test what happen if this line comment out
                 # create adjusted steering measurements for the side camera images
-                correction = 0.6 # this is a parameter to tune
+                correction = 0.4 # this is a parameter to tune
 
                 for offset in range(0, num_samples, batch_size):
                         batch_samples = samples[offset: offset+batch_size]
@@ -59,7 +59,7 @@ def generator(samples, batch_size=32):
                                 images.append(cv2.flip(center_image,1)) 
                                 measurements.append(center_angle*(-1.0))
                                                      
-                            steering_left = center_angle + correction
+                            steering_left = center_angle + correction - 0.1
                             left_name = 'data/IMG/'+batch_sample[1].split('/')[-1]
                             left_image = cv2.imread(left_name)
                             images.append(left_image)
@@ -73,9 +73,9 @@ def generator(samples, batch_size=32):
                             right_image = cv2.imread(right_name)
                             images.append(right_image)
                             measurements.append(steering_right)
-#                             #flip of left cam
-#                             images.append(cv2.flip(right_image,1))
-#                             measurements.append(steering_right*(-1.0))
+#                             #flip of right cam
+  #                           images.append(cv2.flip(right_image,1))
+    #                         measurements.append(steering_right*(-1.0))
                             
                         X_train = np.array(images)
                         Y_train = np.array(measurements)
@@ -138,6 +138,7 @@ model.add(Dense(100))
 model.add(Dropout(rate=0.5))
 model.add(Activation('relu')) 
 model.add(Dense(50))
+# model.add(Dropout(rate=0.5))
 model.add(Activation('relu')) 
 model.add(Dense(10))
 model.add(Dense(1))
